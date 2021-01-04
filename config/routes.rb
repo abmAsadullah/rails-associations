1f92a73b4a0c14c7be07c519b6567d5ee4093966
A B M Asadullah <abmasadullah@gmail.com>
2021-01-04 22:00:20 +0600

    Add many models

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'user/new'
+  get 'user/create'
+  get 'user/show'
+  devise_for :users
+  root to: "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
